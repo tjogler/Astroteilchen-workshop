66ce4c23b8ad67cfa26887ce7bc1056b5433710f
tjogler <tjogler@users.noreply.github.com>
2016-02-24 10:53:10 +0100

    added constants module

---
+++ constants.py
@@ -0,0 +1,53 @@
+#class constants(object):
+
+PI=3.14159    
+PARSEC=3.086e16	# m
+eV=1.602e-19 	# J
+ERG=1.e-7	        # J
+ME= 0.511e6      # eV
+ME_GeV=0.511e-3         # GeV
+MP= 0.938e9          # eV
+MPi0=135e6           #eV
+MPi0_GeV=0.135       #GeV
+MP_GeV = 0.938       #GeV
+HZ_PER_EV=2.145e14
+FLUX_CGS_TO_SI=6.25e15
+H_eV=4.1356692e-15 # eV s
+Hbar=6.626e-34/2./PI 
+H_Erg = 6.63e-27 # erg s
+
+C=3.e8     # m/s
+r0=2.817e-15	# m
+HMASS=1.673e-27	# kg
+MSOL=1.989e30	# kg
+alpha=0.0073	# fine structure const in iso
+eGauss = 4.8e-10 # electron charge in Gaussian units
+
+
+MILLIBARN=1.e-31    # m^2
+sigma_Th = 6.65e-25 # (cm^2) Thomson cross section 8pi / 3 r^2
+
+NA=6.022141e23 # 1/mol
+
+KT_CMB=2.35e-4  # eV
+RHO_CMB=2.60#e11  # eV/cm^3
+KT_DUST=0.02    # eV
+RHO_DUST=2.50e11 # eV/cm^3
+KT_STAR=1.5     # eV
+RHO_STAR=1.0e12  # eV/cm^3
+
+#numeric constants needed for comuting
+epsilon=1.e-15
+
+
+
+# converting constants
+m2cm = 100
+GeV2eV = 1.e9
+erg2eV = 6.24e11
+erg2GeV = 6.24e2
+eV2K = 1.1604e4
+yr2s = 3.15e7 # 365.*24.*3600.       # seconds in a year
+mb2cm2 = 1.e-27 # millibarn to cm^2 conversion
+MeV2erg=1.e-3/erg2GeV    
+TeV2erg=1.e3/erg2GeV
